fd676868cf9d5f67c389c5e960775a91f071e065
nilshartmann <nilshartmann@users.noreply.github.com>
2025-05-19 06:39:56 +0000

    deploy: 3bd587a6ac996c03d8c01621c6a7b2577882779b

---
+++ assets/HelloWorld.stories-C4F6Fer9.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";function d({title:c,subtitle:p}){return o.jsxs("div",{className:"inline-block rounded border p-4 shadow-2xl",children:[o.jsx("span",{className:"font-bold",children:c}),p]})}d.__docgenInfo={description:"",methods:[],displayName:"HelloWorld",props:{title:{required:!0,tsType:{name:"string"},description:""},subtitle:{required:!1,tsType:{name:"string"},description:""}}};const m={component:d},e={args:{title:"Hello",subtitle:"World"}},t={name:"Hello World without subtitle",args:{title:"Hello"}};var r,s,l;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    title: "Hello",
+    subtitle: "World"
+  }
+}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var n,a,i;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: "Hello World without subtitle",
+  args: {
+    title: "Hello"
+  }
+}`,...(i=(a=t.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const h=["helloWorld","withoutTitle"];export{h as __namedExportsOrder,m as default,e as helloWorld,t as withoutTitle};
---
+++ assets/TeaserTitle.stories-Nn_nA_6G.js
@@ -0,0 +1,11 @@
+import{T as c}from"./TeaserTitle-Izj20EZB.js";import"./jsx-runtime-D_zvdyIk.js";const d={component:c},e={args:{children:"Hallo Welt",size:"lg"}},r={args:{children:"Hallo Small",size:"sm"}};var a,s,l;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: "Hallo Welt",
+    size: "lg"
+  }
+}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: "Hallo Small",
+    size: "sm"
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const p=["LargeTitle","SmallTitle"];export{e as LargeTitle,r as SmallTitle,p as __namedExportsOrder,d as default};
